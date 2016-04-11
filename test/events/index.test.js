537fa8bee10ce28ed375e519c234cfaaf2fb67c6
Dan Calacci <dan@dcalacci.net>
2016-04-11 17:13:09 -0400

    start of custom events
    - meetingJoined event

---
+++ test/events/index.test.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+const winston = require('winston')
+
+const app = require('../../src/app')
+const io = require('socket.io-client')
+
+describe('hangout joined event', function (done) {
+  it('creates a participant & meeting when they join for the first time', function (done) {
+    this.timeout(2000)
+
+    var fakeJoinedEvent = {
+      participant: 'joinedParticipantId',
+      meeting: 'meetingName',
+      name: 'fakeParticipantName'
+    }
+
+    var socket = io.connect('http://localhost:3030')
+
+    socket.emit('meetingJoined', fakeJoinedEvent)
+
+    setTimeout(function () {
+      app.service('participants')
+         .find({
+           query: {
+             _id: fakeJoinedEvent.participant
+           }
+         }).then(function (participant) {
+           winston.log('info', '>>> data from joined event:', participant.data[0]._id === fakeJoinedEvent.participant)
+           assert(participant.data[0]._id === fakeJoinedEvent.participant)
+           socket.disconnect()
+           done()
+         }).catch(function (err) {
+           winston.log('info', 'errrrrred', err)
+           socket.disconnect()
+           done(err)
+         })
+    }, 1500)
+  })
+})
