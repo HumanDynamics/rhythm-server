a2742adcb717661ee325c55459deaaf7e5add469
Dan Calacci <dan@dcalacci.net>
2016-04-18 12:30:26 -0400

    basic heartbeat functionality + tests

--- src/events/index.js
+++ src/events/index.js
@@ -4,10 +4,12 @@
 
 const winston = require('winston')
 const meetingJoinedEvent = require('./meetingJoinedEvent')
+const heartbeat = require('./heartbeat')
 
 function configure (socket, app) {
   winston.log('info', 'registering socketio custom events.')
   meetingJoinedEvent.configure(socket, app)
+  heartbeat.configure(socket, app)
 }
 
 module.exports.configure = configure
