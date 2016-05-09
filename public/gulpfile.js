c5c6980dcd0c3246a7273bc71a9b4e65c82a0117
Dan Calacci <dan@dcalacci.net>
2016-05-09 10:37:01 -0400

    static files for landing pages

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -2,9 +2,18 @@ const gulp = require('gulp')
 const less = require('gulp-less')
 const changed = require('gulp-changed')
 const gulpBowerFiles = require('gulp-bower-files')
+const path = require('path')
+const fileinclude = require('gulp-file-include')
+const rename = require('gulp-rename')
+const notify = require('gulp-notify')
 
 // compile into `www` directory
 var dest = './www'
+
+var paths = {
+  templates: './src/templates/',
+  less: 'src/styles/'
+}
 
 // Compiles LESS > CSS
 gulp.task('build-less', function () {
@@ -13,13 +22,30 @@ gulp.task('build-less', function () {
              .pipe(gulp.dest(dest + '/css'))
 })
 
-gulp.task('html', function () {
-  return gulp.src('./src/index.html')
-             .pipe(changed(dest))
+gulp.task('fileinclude', function () {
+  return gulp.src(path.join(paths.templates, '*.template.html'))
+             .pipe(fileinclude({
+               prefix: '@@',
+               basepath: '@root'
+             }))
+             .pipe(rename({
+               extname: ''
+             }))
+             .pipe(rename({
+               extname: '.html'
+             }))
              .pipe(gulp.dest(dest))
+             .pipe(notify({ message: 'Includes: included' }))
 })
 
-gulp.task('img', function () {
+/*
+   gulp.task('html', function () {
+   return gulp.src('./src/index.html')
+   .pipe(changed(dest))
+   .pipe(gulp.dest(dest))
+   }) */
+
+gulp.task('assets', function () {
   return gulp.src('./src/assets/*')
              .pipe(changed(dest + '/assets'))
              .pipe(gulp.dest(dest + '/assets'))
@@ -29,4 +55,4 @@ gulp.task('bower-files', function () {
   gulpBowerFiles().pipe(gulp.dest(dest + '/lib'))
 })
 
-gulp.task('default', ['build-less', 'html', 'bower-files', 'img'])
+gulp.task('default', ['fileinclude', 'build-less', 'bower-files', 'assets'])
