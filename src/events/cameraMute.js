aa6c581aa4d4fa098526c5d86a4f6d049ddaf3b2
Ryan-Neal Mes <ryan.mes@gmail.com>
2016-05-12 14:12:16 -0400

    Added logic to save events to the database

--- src/events/cameraMute.js
+++ src/events/cameraMute.js
@@ -2,12 +2,28 @@
 
 const winston = require('winston')
 
-function saveCameraMuteEvent(event) {
-  winston.log('info', 'Camera Mute event:', event)
+var app;
+var socket;
+
+function saveCameraMuteEvent() {
+  socket.on('cameraMute', function (data) {
+    winston.log('info', 'Camera Mute event:', event)
+
+    app.service('meetingEvents').create({
+      meetingId: data.meetingId,
+      event: 'cameraMute',
+      data: {
+        isCameraMute: data.isCameraMute,
+        participantId: data.participantId
+      }
+      timestamp: new Date()
+    })
+  })
 }
 
 module.exports.configure = function (socket, app) {
-  socket.on('cameraMute', function (data) {
-    saveCameraMuteEvent(data);
-  })
+  app = app;
+  socket = socket;
+
+  saveCameraMuteEvent();
 }
--- src/events/microphoneMute.js
+++ src/events/microphoneMute.js
@@ -2,12 +2,28 @@
 
 const winston = require('winston')
 
-function saveMicrophoneMuteEvent(event) {
-  winston.log('info', 'Microphone Mute event:', event)
+var app;
+var socket;
+
+function saveMicrophoneMuteEvent() {
+  socket.on('microphoneMute', function (data) {
+    winston.log('info', 'Microphone Mute event:', event);
+
+    app.service('meetingEvents').create({
+      meetingId: data.meetingId,
+      event: 'microphoneMute',
+      data: {
+        isMicrophoneMute: data.isMicrophoneMute,
+        participantId: data.participantId
+      }
+      timestamp: new Date()
+    })
+  })
 }
 
 module.exports.configure = function (socket, app) {
-  socket.on('microphoneMute', function (data) {
-    saveMicrophoneMuteEvent(data)
-  })
+  app = app;
+  socket = socket;
+
+  saveMicrophoneMuteEvent();
 }
